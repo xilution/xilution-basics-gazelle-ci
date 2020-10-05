ba257b105be8030cb2b1c94af1db3ee9df3f5fd9
Todd Brunia <tbrunia@gmail.com>
2020-10-05 07:29:03 -0500

    chore: added pipelinetype and reformatted

--- main.tf
+++ main.tf
@@ -11,59 +11,59 @@ data "aws_lambda_function" "metrics-reporter-lambda" {
 # Network (VPN, Subnets, Etc.)
 
 resource "aws_vpc" "xilution_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
   tags = {
-    Name = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-vpc"
+    Name                     = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-vpc"
     xilution_organization_id = var.organization_id
-    originator = "xilution.com"
+    originator               = "xilution.com"
   }
 }
 
 resource "aws_subnet" "xilution_public_subnet_1" {
-  cidr_block = "10.0.0.0/24"
-  vpc_id = aws_vpc.xilution_vpc.id
-  availability_zone = "us-east-1a"
+  cidr_block              = "10.0.0.0/24"
+  vpc_id                  = aws_vpc.xilution_vpc.id
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
-    Name = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-public-subnet-1"
+    Name                     = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-public-subnet-1"
     xilution_organization_id = var.organization_id
-    originator = "xilution.com"
+    originator               = "xilution.com"
   }
 }
 
 resource "aws_subnet" "xilution_public_subnet_2" {
-  cidr_block = "10.0.2.0/24"
-  vpc_id = aws_vpc.xilution_vpc.id
-  availability_zone = "us-east-1b"
+  cidr_block              = "10.0.2.0/24"
+  vpc_id                  = aws_vpc.xilution_vpc.id
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
   tags = {
-    Name = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-public-subnet-2"
+    Name                     = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-public-subnet-2"
     xilution_organization_id = var.organization_id
-    originator = "xilution.com"
+    originator               = "xilution.com"
   }
 }
 
 resource "aws_subnet" "xilution_private_subnet_1" {
-  cidr_block = "10.0.1.0/24"
-  vpc_id = aws_vpc.xilution_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  vpc_id            = aws_vpc.xilution_vpc.id
   availability_zone = "us-east-1a"
   tags = {
-    Name = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-private-subnet-1"
+    Name                     = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-private-subnet-1"
     xilution_organization_id = var.organization_id
-    originator = "xilution.com"
+    originator               = "xilution.com"
   }
 }
 
 resource "aws_subnet" "xilution_private_subnet_2" {
-  cidr_block = "10.0.3.0/24"
-  vpc_id = aws_vpc.xilution_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  vpc_id            = aws_vpc.xilution_vpc.id
   availability_zone = "us-east-1b"
   tags = {
-    Name = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-private-subnet-2"
+    Name                     = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-private-subnet-2"
     xilution_organization_id = var.organization_id
-    originator = "xilution.com"
+    originator               = "xilution.com"
   }
 }
 
@@ -71,23 +71,23 @@ resource "aws_internet_gateway" "xilution_internet_gateway" {
   vpc_id = aws_vpc.xilution_vpc.id
   tags = {
     xilution_organization_id = var.organization_id
-    originator = "xilution.com"
+    originator               = "xilution.com"
   }
 }
 
 resource "aws_eip" "xilution_elastic_ip" {
   tags = {
     xilution_organization_id = var.organization_id
-    originator = "xilution.com"
+    originator               = "xilution.com"
   }
 }
 
 resource "aws_nat_gateway" "xilution_nat_gateway" {
   allocation_id = aws_eip.xilution_elastic_ip.id
-  subnet_id = aws_subnet.xilution_public_subnet_1.id
+  subnet_id     = aws_subnet.xilution_public_subnet_1.id
   tags = {
     xilution_organization_id = var.organization_id
-    originator = "xilution.com"
+    originator               = "xilution.com"
   }
 }
 
@@ -95,72 +95,72 @@ resource "aws_route_table" "xilution_public_route_table" {
   vpc_id = aws_vpc.xilution_vpc.id
   tags = {
     xilution_organization_id = var.organization_id
-    originator = "xilution.com"
+    originator               = "xilution.com"
   }
 }
 
 resource "aws_route" "xilution_public_route" {
-  route_table_id = aws_route_table.xilution_public_route_table.id
+  route_table_id         = aws_route_table.xilution_public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.xilution_internet_gateway.id
+  gateway_id             = aws_internet_gateway.xilution_internet_gateway.id
 }
 
 resource "aws_route_table_association" "xilution_public_route_table_association_1" {
   route_table_id = aws_route_table.xilution_public_route_table.id
-  subnet_id = aws_subnet.xilution_public_subnet_1.id
+  subnet_id      = aws_subnet.xilution_public_subnet_1.id
 }
 
 resource "aws_route_table_association" "xilution_public_route_table_association_2" {
   route_table_id = aws_route_table.xilution_public_route_table.id
-  subnet_id = aws_subnet.xilution_public_subnet_2.id
+  subnet_id      = aws_subnet.xilution_public_subnet_2.id
 }
 
 resource "aws_route_table" "xilution_private_route_table" {
   vpc_id = aws_vpc.xilution_vpc.id
   tags = {
     xilution_organization_id = var.organization_id
-    originator = "xilution.com"
+    originator               = "xilution.com"
   }
 }
 
 resource "aws_route" "xilution_private_route" {
-  route_table_id = aws_route_table.xilution_private_route_table.id
+  route_table_id         = aws_route_table.xilution_private_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.xilution_nat_gateway.id
+  nat_gateway_id         = aws_nat_gateway.xilution_nat_gateway.id
 }
 
 resource "aws_route_table_association" "xilution_private_route_table_association_1" {
   route_table_id = aws_route_table.xilution_private_route_table.id
-  subnet_id = aws_subnet.xilution_private_subnet_1.id
+  subnet_id      = aws_subnet.xilution_private_subnet_1.id
 }
 
 resource "aws_route_table_association" "xilution_private_route_table_association_2" {
   route_table_id = aws_route_table.xilution_private_route_table.id
-  subnet_id = aws_subnet.xilution_private_subnet_2.id
+  subnet_id      = aws_subnet.xilution_private_subnet_2.id
 }
 
 # Metrics
 
 resource "aws_lambda_permission" "allow-gazelle-cloudwatch-every-ten-minute-event-rule" {
-  action = "lambda:InvokeFunction"
+  action        = "lambda:InvokeFunction"
   function_name = data.aws_lambda_function.metrics-reporter-lambda.function_name
-  principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.gazelle-cloudwatch-every-ten-minute-event-rule.arn
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.gazelle-cloudwatch-every-ten-minute-event-rule.arn
 }
 
 resource "aws_cloudwatch_event_rule" "gazelle-cloudwatch-every-ten-minute-event-rule" {
-  name = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-cloudwatch-event-rule"
+  name                = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-cloudwatch-event-rule"
   schedule_expression = "rate(10 minutes)"
-  role_arn = data.aws_iam_role.cloudwatch-events-rule-invocation-role.arn
+  role_arn            = data.aws_iam_role.cloudwatch-events-rule-invocation-role.arn
   tags = {
     xilution_organization_id = var.organization_id
-    originator = "xilution.com"
+    originator               = "xilution.com"
   }
 }
 
 resource "aws_cloudwatch_event_target" "gazelle-cloudwatch-event-target" {
-  rule = aws_cloudwatch_event_rule.gazelle-cloudwatch-every-ten-minute-event-rule.name
-  arn = data.aws_lambda_function.metrics-reporter-lambda.arn
+  rule  = aws_cloudwatch_event_rule.gazelle-cloudwatch-every-ten-minute-event-rule.name
+  arn   = data.aws_lambda_function.metrics-reporter-lambda.arn
   input = <<-DOC
   {
     "Environment": "prod",
