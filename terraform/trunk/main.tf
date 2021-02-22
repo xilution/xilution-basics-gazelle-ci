f717bc632d8192cb22301e3877a1bc08ecd39c16
Todd Brunia <tbrunia@gmail.com>
2021-02-22 10:51:40 -0600

    feat: removed private subnets

--- terraform/trunk/main.tf
+++ terraform/trunk/main.tf
@@ -45,28 +45,6 @@ resource "aws_subnet" "xilution_public_subnet_2" {
   }
 }
 
-resource "aws_subnet" "xilution_private_subnet_1" {
-  cidr_block        = "10.0.1.0/24"
-  vpc_id            = aws_vpc.xilution_vpc.id
-  availability_zone = "us-east-1a"
-  tags = {
-    Name                     = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-private-subnet-1"
-    xilution_organization_id = var.organization_id
-    originator               = "xilution.com"
-  }
-}
-
-resource "aws_subnet" "xilution_private_subnet_2" {
-  cidr_block        = "10.0.3.0/24"
-  vpc_id            = aws_vpc.xilution_vpc.id
-  availability_zone = "us-east-1b"
-  tags = {
-    Name                     = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-private-subnet-2"
-    xilution_organization_id = var.organization_id
-    originator               = "xilution.com"
-  }
-}
-
 resource "aws_internet_gateway" "xilution_internet_gateway" {
   vpc_id = aws_vpc.xilution_vpc.id
   tags = {
@@ -76,15 +54,6 @@ resource "aws_internet_gateway" "xilution_internet_gateway" {
 }
 
 resource "aws_eip" "xilution_elastic_ip" {
-  tags = {
-    xilution_organization_id = var.organization_id
-    originator               = "xilution.com"
-  }
-}
-
-resource "aws_nat_gateway" "xilution_nat_gateway" {
-  allocation_id = aws_eip.xilution_elastic_ip.id
-  subnet_id     = aws_subnet.xilution_public_subnet_1.id
   tags = {
     xilution_organization_id = var.organization_id
     originator               = "xilution.com"
@@ -113,30 +82,6 @@ resource "aws_route_table_association" "xilution_public_route_table_association_
 resource "aws_route_table_association" "xilution_public_route_table_association_2" {
   route_table_id = aws_route_table.xilution_public_route_table.id
   subnet_id      = aws_subnet.xilution_public_subnet_2.id
-}
-
-resource "aws_route_table" "xilution_private_route_table" {
-  vpc_id = aws_vpc.xilution_vpc.id
-  tags = {
-    xilution_organization_id = var.organization_id
-    originator               = "xilution.com"
-  }
-}
-
-resource "aws_route" "xilution_private_route" {
-  route_table_id         = aws_route_table.xilution_private_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.xilution_nat_gateway.id
-}
-
-resource "aws_route_table_association" "xilution_private_route_table_association_1" {
-  route_table_id = aws_route_table.xilution_private_route_table.id
-  subnet_id      = aws_subnet.xilution_private_subnet_1.id
-}
-
-resource "aws_route_table_association" "xilution_private_route_table_association_2" {
-  route_table_id = aws_route_table.xilution_private_route_table.id
-  subnet_id      = aws_subnet.xilution_private_subnet_2.id
 }
 
 # Metrics
